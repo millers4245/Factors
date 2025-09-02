3b1bad93afcf0cc3e2f6a45a7854ca442f9d9a21
millers4245 <millers4245@masseyhigh.school.nz>
2025-09-03 09:55:44 +1200

    Add files via upload

---
+++ B_01_factors.py
@@ -0,0 +1,52 @@
+# Generates headings ( ----- Heading ---- )
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+# Displays instructions
+def instructions():
+    statement_generator("Instructions", "-")
+
+    print('''
+Factors
+- Enter a number between 1 and 200 
+- Get your number factorized
+- Type 'xxx' to stop program
+
+    ''')
+
+def num_check(question):
+    error = "Please enter a number that is between 1 and 200 inclusive\n"
+    while True:
+
+        response = input(question).lower()
+        if response == "xxx":
+            return response
+
+        try:
+            response = int(response)
+
+            if 1 <= response <= 200:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine
+
+statement_generator("The Ultimate Factor Finder", "-")
+
+want_instructions= input("\nPress <enter> to read the instructions "
+                         "or any key to continue")
+
+if want_instructions == "":
+    instructions()
+
+while True:
+    to_factor = num_check("To factor: ")
+    print("You chose to factor", to_factor)
+
+    if to_factor == "xxx":
+        break
